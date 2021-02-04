d9ad2b99ec926058370c0465421324e560175b9f
raaaahman <sylvain@devindetails.com>
2021-02-04 08:14:44 +0100

    Static version of the animal cards

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import elephantImg from './assets/elephant.png'
+import giraffeImg from './assets/giraffe.png'
+import hippoImg from './assets/hippo.png'
+import monkeyImg from './assets/monkey.png'
+import pandaImg from './assets/panda.png'
+import parrotImg from './assets/parrot.png'
+import penguinImg from './assets/penguin.png'
+import pigImg from './assets/pig.png'
+import rabbitImg from './assets/rabbit.png'
+import snakeImg from './assets/snake.png'
+
+const images = [
+  { alt: 'Elephant' , src: elephantImg },
+  { alt: 'Giraffe', src: giraffeImg },
+  { alt: 'Hippopotamus', src: hippoImg },
+  { alt: 'Monkey', src: monkeyImg },
+  { alt: 'Panda', src: pandaImg },
+  { alt: 'Parrot', src: parrotImg },
+  { alt: 'Penguin', src: penguinImg },
+  { alt: 'Pig', src: pigImg },
+  { alt: 'Rabbit', src: rabbitImg },
+  { alt: 'Snake', src: snakeImg}
+]
 
 function App() {
+  const pairedImages = images.concat(images)
+  const shuffledImages = pairedImages.sort(() => Math.random() - 0.5)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+      {shuffledImages.map((image, index) => {
+        return (
+          <figure 
+            key={index} 
+            style={{
+              boxSizing: 'border-box',
+              width: '160px', 
+              height: '160px',
+              borderRadius: '16px', 
+              border: '12px solid brown',
+              display: 'flex', 
+              justifyContent: 'center',
+              alignItems: 'center',
+              backgroundColor: 'beige' 
+            }}
+          >
+            <img 
+              src={image.src} 
+              alt={image.alt} 
+              style={{
+                maxWidth: '128px', 
+                maxHeight: '128px'
+              }}
+            />
+          </figure>
+        )
+      })}      
     </div>
   );
 }

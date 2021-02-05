091b8f0324426e8a375d571c0a9585ebdeb59a05
raaaahman <sylvain@devindetails.com>
2021-02-05 07:20:54 +0100

    feat: Use State Hook to flip cards

--- src/Card.js
+++ src/Card.js
@@ -1,28 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/logo.svg'
+
 function Card({ image }) {
-    return (
-        <figure 
-            style={{
-              boxSizing: 'border-box',
-              width: '160px', 
-              height: '160px',
-              borderRadius: '16px', 
-              border: '12px solid brown',
-              display: 'flex', 
-              justifyContent: 'center',
-              alignItems: 'center',
-              backgroundColor: 'beige' 
-            }}
-          >
-            <img 
-              src={image.src} 
-              alt={image.alt} 
-              style={{
-                maxWidth: '128px', 
-                maxHeight: '128px'
-              }}
-            />
-        </figure>
-    )
+  const [ isFlipped, setFlipped ] = useState(false)
+
+  const displayImg = isFlipped ? image : { src: reactLogo, alt: 'A card to flip' }
+  
+  return (
+    <figure
+      onClick={() => setFlipped(true)}
+      style={{
+        boxSizing: 'border-box',
+        width: '160px', 
+        height: '160px',
+        borderRadius: '16px', 
+        border: '12px solid brown',
+        display: 'flex', 
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'beige' 
+      }}
+    >
+      <img 
+        src={displayImg.src} 
+        alt={displayImg.alt}
+        style={{
+          maxWidth: '128px', 
+          maxHeight: '128px'
+        }}
+      />
+    </figure>
+  )
 }
 
 export default Card
